5857a2dba068a0cb06fc0dccae4bd30eec8eb693
WillBeI <9935ok9938@gmail.com>
2020-12-02 15:40:22 +0900

    4장

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/customer';
+
+const customer = {
+  'name' : '양현용',
+  'birthday' : '961204',
+  'gender' : '남자',
+  'job' : '대학생'
+}
 
 function App(){
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+      
+    />
   );
 }
 
