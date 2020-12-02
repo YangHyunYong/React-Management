4efa3217942885636f04e9bc9aebb96e327effb5
WillBeI <9935ok9938@gmail.com>
2020-12-02 15:57:56 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 import Customer from './components/customer';
 
-const customer = {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
   'name' : '양현용',
   'birthday' : '961204',
   'gender' : '남자',
   'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '손태민',
+  'birthday' : '961223',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '강희진',
+  'birthday' : '960309',
+  'gender' : '여자',
+  'job' : '대학생'
 }
+]
 
 function App(){
   return (
-    <Customer
-      name={customer.name}
-      birthday={customer.birthday}
-      gender={customer.gender}
-      job={customer.job}
-      
-    />
+    <div>
+    {
+      customers.map(c=>{
+        return <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday}
+          gender={c.gender}
+          job={c.job}
+        />
+      })
+    }
+    </div>
   );
 }
 
